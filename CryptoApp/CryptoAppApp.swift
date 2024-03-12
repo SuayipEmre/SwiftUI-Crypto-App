362ba736daa9009ab1196556ab13683b765637a3
Suayip Emre <suayipemredev@gmail.com>
2024-03-12 20:31:17 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Şuayip Emre on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
