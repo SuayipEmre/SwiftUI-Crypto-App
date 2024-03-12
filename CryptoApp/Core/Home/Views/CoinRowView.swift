98db55bddc65f9a2a38c0ef630e7e6e2b5a6499a
Suayip Emre <suayipemredev@gmail.com>
2024-03-12 23:55:40 +0300

    style: design a row to display coins in a list

---
+++ CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,78 @@
+//
+//  CoinRowView.swift
+//  CryptoApp
+//
+//  Created by Şuayip Emre on 12.03.2024.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin : CoinModel
+    
+    let showHoldingsColumn : Bool
+    
+    var body: some View {
+        HStack(spacing:0){
+            leftColumn
+            Spacer()
+            
+            if showHoldingsColumn{
+               centerColumn
+            }
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+#Preview {
+    CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingsColumn: true)
+        .previewLayout(.sizeThatFits)
+}
+
+
+
+extension CoinRowView{
+    
+    private var leftColumn : some View{
+        HStack(spacing:0){
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundStyle(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn : some View{
+        VStack(alignment:.trailing){
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundStyle(Color.theme.accent)
+    }
+    
+    private var rightColumn : some View{
+        VStack(alignment:.trailing){
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundStyle(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "" )
+                .foregroundStyle(
+                    (coin.priceChange24H ?? 0) >= 0  ?
+                    Color.theme.green :
+                    Color.theme.green
+                )
+            
+            
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
